1a9da31fb3d0c2c1c7da6fff0c53f4c5fb9dacc1
Vasanth <vasanthvasanth4863@gmail.com>
2026-06-23 07:55:27 +0530

    Implement Fibonacci sequence generator in Java

---
+++ fibonacci.java
@@ -0,0 +1,15 @@
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a= 0;
+        int b= 1;
+        for(int i=1;i<=n;i++){
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        scan.close();
+    }
+}
