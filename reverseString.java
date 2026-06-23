f890ebf3e54355ba344a03b27eef238d7f9ed338
Vasanth <vasanthvasanth4863@gmail.com>
2026-06-23 07:50:00 +0530

    Add reverse string functionality in reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        String rev = "";
+        for(int i=str.length()-1;i>=0;i--){
+            rev = rev + str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
+//pali
