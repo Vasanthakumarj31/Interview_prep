abf8e086af0d68c99d930e312d894e93c3e4bdac
Vasanth <vasanthvasanth4863@gmail.com>
2026-06-23 07:57:55 +0530

    Add vowel counting functionality in vowels.java

---
+++ vowels.java
@@ -0,0 +1,15 @@
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        int count = 0;
+        for(int i = 0;i<str.length();i++){
+            char ch  = str.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                count++;
+            }
+
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
